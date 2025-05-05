76ed6511acc2855c488eb416286ca4826ee64399
Nicholas Earl <contact@nicholasearl.me>
2025-05-05 12:04:57 -0500

    Fix convergence check

--- src/feadme/cli.py
+++ src/feadme/cli.py
@@ -107,7 +107,7 @@ def run(
         # if "ZTF" not in str(template_path):
         #     continue
 
-        # if "ZTF18aahfohe" not in str(template_path):  # ZTF18aahfohe
+        # if "ZTF18aaaovpz" not in str(template_path):  # ZTF18aahfohe
         #     continue
 
         # Load the template
@@ -166,7 +166,7 @@ def run(
             progress_bar=not no_progress_bar,
         )
 
-        if not nuts_sampler.converged:
+        if not nuts_sampler.check_convergence():
             nuts_sampler.sample(init_strategy=init_to_median(num_samples=1000))
             nuts_sampler.write_results()
         else:
