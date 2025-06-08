Fix for circular parameter stats calculation

src/feadme/plotting.py
@@ -65,7 +65,7 @@ def plot_model_fit(
     output_dir,
     label,
 ):
-    fig, ax = plt.subplots(layout='constrained')
+    fig, ax = plt.subplots(layout="constrained")
     ax.errorbar(
         wave, flux, yerr=flux_err, fmt="o", color="grey", zorder=-10, alpha=0.25
     )
@@ -90,9 +90,11 @@ def plot_model_fit(
         if "_flux" not in var
     }
 
-    new_wave = np.linspace(
-        wave.min(), wave.max(), num=1000
-    )
+    for prof in template.all_profiles:
+        for param in prof._fixed():
+            res_pars[f"{prof.name}_{param.name}"] = param.value
+
+    new_wave = np.linspace(wave.min(), wave.max(), num=1000)
 
     res_flux, res_disk_flux, res_line_flux = evaluate_disk_model(
         template, new_wave, res_pars

src/feadme/samplers/base_sampler.py
@@ -306,7 +306,9 @@ class Sampler(ABC):
     def _calculate_rhat(self, param: str, stats: Dict) -> float:
         """Calculate R-hat statistic for parameter."""
         if param.endswith("apocenter"):
-            return circular_rhat(self._idata.posterior[param].values)
+            return circular_rhat(
+                self._idata.posterior[param].stack(sample=("chain", "draw")).values
+            )
         return np.mean(stats["r_hat"])
 
     def _get_results_summary(self) -> Table: