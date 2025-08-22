223fb109f9d7a721292a74d12c5e8dda5cd10227
Nicholas Earl <contact@nicholasearl.me>
2025-08-22 07:45:05 -0500

    Improve neutra reparameterization learning

--- src/feadme/samplers/nuts_sampler.py
+++ src/feadme/samplers/nuts_sampler.py
@@ -7,6 +7,7 @@ from numpyro.infer.autoguide import (
     AutoBNAFNormal,
     AutoIAFNormal,
     AutoMultivariateNormal,
+    AutoDAIS,
 )
 from numpyro.infer.reparam import NeuTraReparam
 from numpyro import optim
@@ -37,12 +38,12 @@ class NUTSSampler(BaseSampler):
         Run the NUTS sampler to perform MCMC sampling.
         """
         rng_key = random.PRNGKey(int(time.time() * 1000) % 2**32)
-        rng_key, svi_key = random.split(rng_key)
+        rng_key, svi_key, mcmc_key = random.split(rng_key, 3)
 
-        # guide = AutoBNAFNormal(self.model, hidden_factors=[8, 8], num_flows=1)
+        guide = AutoBNAFNormal(self.model, hidden_factors=[8, 8], num_flows=2)
         # guide = AutoIAFNormal(self.model, hidden_dims=[32, 32], num_flows=2)
-        guide = AutoMultivariateNormal(self.model)
-        optimizer = optim.Adam(lambda t: 0.005 * 0.999**t)
+        # guide = AutoMultivariateNormal(self.model)
+        optimizer = optim.Adam(0.003)
         svi = SVI(self.model, guide, optimizer, Trace_ELBO())
         svi_result = svi.run(
             svi_key,
@@ -53,23 +54,39 @@ class NUTSSampler(BaseSampler):
             flux_err=self.flux_err,
         )
 
-        # Check convergence
-        if (
-            jnp.std(svi_result.losses[-1000:])
-            / jnp.abs(jnp.mean(svi_result.losses[-1000:]))
-            > 0.01
-        ):
-            logger.warning("SVI may not have converged!")
+        # Convergence check
+        recent_losses = svi_result.losses[-1000:]
+        relative_std = jnp.std(recent_losses) / jnp.abs(jnp.mean(recent_losses))
+        
+        if relative_std > 0.01:
+            logger.warning(f"SVI may not have converged! Relative std: {relative_std:.4f}")
+            # Could add logic to extend SVI or use simpler guide
+        else:
+            logger.info(f"SVI converged successfully. Final loss: {svi_result.losses[-1]:.4f}")
 
         neutra = NeuTraReparam(guide, svi_result.params)
         neutra_model = neutra.reparam(self.model)
 
         # Initialize from VI posterior
-        init_params = guide.sample_posterior(rng_key, svi_result.params)
+        init_key, mcmc_key = random.split(mcmc_key)
+        init_params = guide.sample_posterior(init_key, svi_result.params, sample_shape=(self.sampler.num_chains,))
+
+        if self.sampler.num_chains > 1:
+            # Sample one set of parameters per chain
+            init_params = guide.sample_posterior(
+                init_key, 
+                svi_result.params, 
+                sample_shape=(self.sampler.num_chains,)
+            )
+            # init_params now has shape (num_chains, ...) for each parameter
+            chain_init_params = init_params
+        else:
+            # Single chain - sample one set of parameters
+            chain_init_params = guide.sample_posterior(init_key, svi_result.params)
 
         kernel = NUTS(
             neutra_model,
-            init_strategy=init_to_median(),
+            init_strategy=init_to_value(values=chain_init_params),
             target_accept_prob=self.sampler.target_accept_prob,
             max_tree_depth=self.sampler.max_tree_depth,
             dense_mass=self.sampler.dense_mass,
